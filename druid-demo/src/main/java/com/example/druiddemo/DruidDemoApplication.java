dff1f8959809b01101f45cf082f5c271232ec8f1
kongming <komiles@163.com>
2020-05-12 19:54:27 +0800

    *修改数据

--- druid-demo/src/main/java/com/example/druiddemo/DruidDemoApplication.java
+++ druid-demo/src/main/java/com/example/druiddemo/DruidDemoApplication.java
@@ -2,8 +2,14 @@ package com.example.druiddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * 去掉自动数据库自动配置类
+ * @author komiles@163.com
+ * @date 2020-05-12 18:41
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DruidDemoApplication {
 
     public static void main(String[] args) {
