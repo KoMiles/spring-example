62364ef16b6b1a0a9ab6d0834eb701d63329fd86
kongming <komiles@163.com>
2021-04-28 17:25:05 +0800

    *修改数据

--- elasticsearch-demo/src/main/java/com/example/elasticsearchdemo/controller/UserController.java
+++ elasticsearch-demo/src/main/java/com/example/elasticsearchdemo/controller/UserController.java
@@ -11,7 +11,6 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
